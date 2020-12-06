dd6f4e68b183d76370ff75c24054557eedb12dc7
Fares <faresamiar1@gmail.com>
2020-12-06 22:02:23 +0100

    Correction rangée
    mise en place liste pour les lots dans la  rangée

--- src/appli/Meuble.java
+++ src/appli/Meuble.java
@@ -25,7 +25,6 @@ public class Meuble {
 		
 	}
 
-
 	public double getPrix() {
 		return prix;
 	}
--- src/appli/Rangee.java
+++ src/appli/Rangee.java
@@ -1,30 +1,36 @@
 package appli;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Rangee {
 	
 	private int longueur;
 	private final int LARGEUR = 1, HAUTEUR = 1;
 	
-	private Lot[] lotsRangee;
+	private Map<Lot,Integer> lotsRangee;
 
 	public void setLongueur(int longueur) {
 		this.longueur = longueur;
-		lotsRangee = new Lot[longueur];
+		lotsRangee = new HashMap<>();
 	}
 	
+	
 	public void ajouterLot(Lot lot) {
-		int espace = 0;
-		for(int i = 0; i < lotsRangee.length; ++i) {
-			if(lotsRangee[i] == null)
-				++espace;
-			if(espace >= lot.getVolume())
-				for(int j = 0; j < i; ++j) {
-					lotsRangee[j] = lot; 
-					return;
-				}
-		}
+		if(!estRempli())
+			lotsRangee.put(lot, lot.getVolume());
+	}
+	
+	public boolean estRempli() {
+		int volumes = 0;
+		if(!lotsRangee.isEmpty())
+			for(Entry e : lotsRangee.entrySet())
+				volumes += (int) e.getValue();
+		return volumes < longueur;
 	}
 	
 	
