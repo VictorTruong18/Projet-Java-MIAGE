69b3b16096819b3c37d30b29716ff4f76dd6365f
Fares <faresamiar1@gmail.com>
2020-12-12 16:12:16 +0100

    Repassage au tableau pour rangée

--- src/appli/Rangee.java
+++ src/appli/Rangee.java
@@ -14,42 +14,46 @@ public class Rangee {
 	private int longueur;
 	private final int LARGEUR = 1, HAUTEUR = 1;
 	
-	private List<Lot> lotsRangee;
+	private Lot[] lotsRangee;
 	
 	public Rangee(int longueur) {
 		this.longueur = longueur;
-		lotsRangee = new ArrayList<Lot>(longueur);
+		lotsRangee = new Lot[longueur];
 		System.out.println(lotsRangee);
 	}
 
 
 	
 	public void ajouterLot(Lot lot) {
-		lotsRangee.add(lot);
+		int espace = 0;
+		for(int i = 0; i < lotsRangee.length; ++i) {
+			if(lotsRangee[i] == null)
+				++espace;
+			if(espace >= lot.getVolume())
+				for(int j = 0; j < i; ++j) {
+					lotsRangee[j] = lot; 
+					return;
+				}
+		}
 	}
 	
 	public boolean estRempli() {
 		int volumes = 0;
-		if(!lotsRangee.isEmpty())
+		if(lotsRangee.length != 0)
 			for(Lot l : lotsRangee)
 				volumes += l.getVolume();
 		return volumes < longueur;
 	}
 	
 	public boolean placeDisponible(Lot l) {
-		Iterator it = this.lotsRangee.iterator();
-		int counter = 0;
-		while(it.hasNext()) {
-			if(it.next() == null) {
-				counter++;
-				if(counter == l.getVolume()) {
-					return true;
-				}
-			} else {
-				counter=0;
-			}
-		}
-		return false;
+		if(estRempli())
+			return false;
+		int volumes = 0;
+		
+		for(Lot lot : lotsRangee)
+			volumes += l.getVolume();
+		
+		return (longueur - volumes) >= l.getVolume() ;
 	}
 	
 
--- src/appli/main.java
+++ src/appli/main.java
@@ -7,15 +7,15 @@ public class main {
 
 	public static void main(String[] args) {
 //		//Initialisation de l'Entrepot
-//		Entrepot entrepot = new Entrepot();
-//		//Creation de deux rangees de volume 5
-//		Rangee rangee1 = new Rangee(5);
-//		Rangee rangee2 = new Rangee(5);
+		Entrepot entrepot = new Entrepot();
+		//Creation de deux rangees de volume 5
+		Rangee rangee1 = new Rangee(5);
+		Rangee rangee2 = new Rangee(5);
 //		//Creation d'un lot de volume 6
-//		Lot planchesDeBoie = new Lot(1,"PlanchesDeBoie",10,145.00,6);
+		Lot planchesDeBoie = new Lot(1,"PlanchesDeBoie",10,145.00,6);
 //		//L'Entrepot ajoute les rangees
-//		entrepot.ajouterRangee(rangee1);
-//		entrepot.ajouterRangee(rangee2);
+		entrepot.ajouterRangee(rangee1);
+		entrepot.ajouterRangee(rangee2);
 //		entrepot.ajouterUnLot(planchesDeBoie);		
 		
 	}
